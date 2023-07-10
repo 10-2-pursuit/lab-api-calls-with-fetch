1be91247a308c64badfec6b6e8a1e1433d444946
Shanice Griffin <shanicegriffin@Shanices-MacBook-Air.local>
2023-07-09 21:13:34 -0400

    finished based requirements

--- script.js
+++ script.js
@@ -16,15 +16,29 @@ sumbitButton.addEventListener("click", (e) => {
 
 const showQuestion = (json) => {
     let results = json.results
-    console.log(results)
+    let questionsHTML = '';
     
-    for (let key in results) {
-        centered.innerHTML = `
+    for (let i = 0; i < results.length; i++) {
+        questionsHTML += `
         <article class="card">
-            <h2>${key.category}</h2>
-            <p>${key.question}</p>
-            <button>Show Answer</button>
-            <p class="hidden">${key.correct_answer}</p>
-        </article>`
+            <h2>${results[i].category}</h2>
+            <p>${results[i].question}</p>
+            <button class="show-answer-btn">Show Answer</button>
+            <p class="hidden">${results[i].correct_answer}</p>
+        </article>`;
     }
+    centered.innerHTML = questionsHTML;
+    
+    document.addEventListener("click", (e) => {
+        if (e.target.classList.contains("show-answer-btn")) {
+            const answerElement = e.target.nextElementSibling;
+            answerElement.classList.toggle("hidden");
+    
+            if (answerElement.classList.contains("hidden")) {
+                e.target.textContent = "Show Answer";
+            } else {
+                e.target.textContent = "Hide Answer";
+            }
+        }
+    });
 }
